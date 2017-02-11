b74398802887b8cc9576917a4dc89f7d2ca95e0d
OHMAE Ryosuke <ryo@mm2d.net>
2017-02-11 18:46:39 +0900

    TextParseUtilsの機能拡張

--- lib/src/main/java/net/mm2d/util/TextParseUtils.java
+++ lib/src/main/java/net/mm2d/util/TextParseUtils.java
@@ -16,38 +16,71 @@ import javax.annotation.Nullable;
  */
 public class TextParseUtils {
     /**
-     * 文字列をint値としてパースする。パースできなかった場合はデフォルト値を返す。
+     * 与えられた文字列を10進数としてint値にパースする。
      *
-     * @param string       パースする文字列
+     * <p>パースできなかった場合はデフォルト値を返す。
+     *
+     * @param value        パースする文字列
      * @param defaultValue デフォルト値
      * @return パース結果
      */
-    public static int parseIntSafely(@Nullable String string, int defaultValue) {
-        if (TextUtils.isEmpty(string)) {
-            return defaultValue;
-        }
-        try {
-            return Integer.parseInt(string);
-        } catch (final NumberFormatException ignored) {
-        }
-        return defaultValue;
+    public static int parseIntSafely(@Nullable String value, int defaultValue) {
+        return parseIntSafely(value, 10, defaultValue);
     }
 
     /**
-     * 文字列をlong値としてパースする。パースできなかった場合はデフォルト値を返す。
+     * 与えられた文字列をradix進数としてint値にパースする。
      *
-     * @param string       パースする文字列
+     * <p>パースできなかった場合はデフォルト値を返す。
+     *
+     * @param value        パースする文字列
+     * @param radix        パースする文字列の基数
      * @param defaultValue デフォルト値
      * @return パース結果
      */
-    public static long parseLongSafely(@Nullable String string, long defaultValue) {
-        if (TextUtils.isEmpty(string)) {
+    public static int parseIntSafely(@Nullable String value, int radix, int defaultValue) {
+        if (TextUtils.isEmpty(value)) {
             return defaultValue;
         }
         try {
-            return Long.parseLong(string);
-        } catch (final NumberFormatException ignored) {
+            return Integer.parseInt(value, radix);
+        } catch (final NumberFormatException e) {
+            return defaultValue;
         }
-        return defaultValue;
     }
+
+    /**
+     * 与えられた文字列を10進数としてlong値にパースする。
+     *
+     * <p>パースできなかった場合はデフォルト値を返す。
+     *
+     * @param value        パースする文字列
+     * @param defaultValue デフォルト値
+     * @return パース結果
+     */
+    public static long parseLongSafely(@Nullable String value, long defaultValue) {
+        return parseLongSafely(value, 10, defaultValue);
+    }
+
+    /**
+     * 与えられた文字列をradix進数としてint値にパースする。
+     *
+     * <p>パースできなかった場合はデフォルト値を返す。
+     *
+     * @param value        パースする文字列
+     * @param radix        パースする文字列の基数
+     * @param defaultValue デフォルト値
+     * @return パース結果
+     */
+    public static long parseLongSafely(@Nullable String value, int radix, long defaultValue) {
+        if (TextUtils.isEmpty(value)) {
+            return defaultValue;
+        }
+        try {
+            return Long.parseLong(value, radix);
+        } catch (final NumberFormatException e) {
+            return defaultValue;
+        }
+    }
+
 }
