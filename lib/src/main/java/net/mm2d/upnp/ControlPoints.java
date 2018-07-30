b0b1b306a6c1cee1ae8cb9aa02f22b20605b813d
OHMAE Ryosuke <ryo@mm2d.net>
2018-07-30 19:04:45 +0900

    feature: add EmptyControlPoint

---
+++ lib/src/main/java/net/mm2d/upnp/ControlPoints.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2018 大前良介 (OHMAE Ryosuke)
+ *
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/MIT
+ */
+
+package net.mm2d.upnp;
+
+public class ControlPoints {
+    public static final ControlPoint EMPTY_CONTROL_POINT = new EmptyControlPoint();
+}
