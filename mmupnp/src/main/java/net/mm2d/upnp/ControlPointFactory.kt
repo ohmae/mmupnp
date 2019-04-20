204c3ca6acf43ced9be793e3d571f27a1c691392
OHMAE Ryosuke <ryo@mm2d.net>
2019-04-21 00:07:06 +0900

    :+1: add builder

--- mmupnp/src/main/java/net/mm2d/upnp/ControlPointFactory.kt
+++ mmupnp/src/main/java/net/mm2d/upnp/ControlPointFactory.kt
@@ -13,25 +13,30 @@ import net.mm2d.upnp.internal.impl.DiFactory
 import java.net.NetworkInterface
 
 /**
- * ControlPointのインスタンスを作成するFactory。
- *
- * 将来的にControlPointもインターフェースに変更するため、
- * ControlPointのコンストラクタはDeprecatedとしている。
+ * Factory to create an instance of ControlPoint.
  *
  * @author [大前良介 (OHMAE Ryosuke)](mailto:ryo@mm2d.net)
  */
 object ControlPointFactory {
     /**
-     * ControlPointのインスタンスを作成する。
+     * Create an instance of ControlPoint.
      *
-     * @param protocol 使用するプロトコルスタック。未指定の場合デフォルトのプロトコルスタックが使用される
-     * @param interfaces 使用するインターフェース。未指定の場合、プロトコルスタックから自動選択される。
-     * @param callbackExecutor コールバックを適切なスレッドで実行する[TaskExecutor]。
-     * コールバックのスレッドを指定したい場合に指定する。未指定の場合singleThreadのExecutorが使用される。
-     * @param callbackHandler コールバックを適切なスレッドで実行する。[callbackExecutor]の指定の方が優先される。
-     * @param notifySegmentCheckEnabled SSDP Notifyパケットを受け取った時にセグメントチェックを行う。
-     * @return ControlPointのインスタンス
-     * @throws IllegalStateException 使用可能なインターフェースがない。
+     * @param protocol protocol stack.
+     * If not specified, the default protocol stack is used.
+     * @param interfaces network interface.
+     * If not specified, it is automatically selected from the protocol stack.
+     * @param callbackExecutor callback executor.
+     * All callbacks are executed from the callback thread.
+     * This argument is used to specify the callback thread.
+     * If not specified, single Thread Executor is used.
+     * @param callbackHandler callback handler.
+     * This is used as a TaskExecutor.
+     * All callbacks are executed from the callback thread.
+     * This argument is used to specify the callback thread.
+     * If not specified, single Thread Executor is used.
+     * @param notifySegmentCheckEnabled Set whether to check the segment of SSDP Notify packet or not.
+     * @return Instance of ControlPoint.
+     * @throws IllegalStateException There is no interface available.
      */
     @JvmStatic
     fun create(
@@ -58,5 +63,103 @@ object ControlPointFactory {
         return if (interfaces?.none() != false) {
             protocol.availableInterfaces
         } else interfaces
+    }
+
+    /**
+     * Return Builder instance.
+     */
+    @JvmStatic
+    fun builder(): ControlPointBuilder = ControlPointBuilder()
+
+    /**
+     * Builder for ControlPoint.
+     *
+     * Mainly assuming to be use from Java.
+     */
+    class ControlPointBuilder internal constructor() {
+        private var protocol: Protocol = Protocol.DEFAULT
+        private var interfaces: Iterable<NetworkInterface>? = null
+        private var callbackExecutor: TaskExecutor? = null
+        private var notifySegmentCheckEnabled: Boolean = false
+
+        /**
+         * Set protocol stack.
+         *
+         * If not specified, the default protocol stack is used.
+         *
+         * @param proto Protocol stack
+         * @return builder
+         */
+        fun setProtocol(proto: Protocol): ControlPointBuilder {
+            protocol = proto
+            return this
+        }
+
+        /**
+         * Set network interface.
+         *
+         * If not specified, it is automatically selected from the protocol stack.
+         *
+         * @param ifs Network interface
+         * @return builder
+         */
+        fun setInterfaces(ifs: Iterable<NetworkInterface>): ControlPointBuilder {
+            interfaces = ifs
+            return this
+        }
+
+        /**
+         * Set callback executor.
+         *
+         * All callbacks are executed from the callback thread.
+         * This argument is used to specify the callback thread.
+         * If not specified, single Thread Executor is used.
+         *
+         * @param executor callback executor
+         * @return builder
+         */
+        fun setCallbackExecutor(executor: TaskExecutor): ControlPointBuilder {
+            callbackExecutor = executor
+            return this
+        }
+
+        /**
+         * Set callback handler.
+         *
+         * This is used as a TaskExecutor.
+         * All callbacks are executed from the callback thread.
+         * This argument is used to specify the callback thread.
+         * If not specified, single Thread Executor is used.
+         *
+         * @param handler callback executor
+         * @return builder
+         */
+        fun setCallbackHandler(handler: (Runnable) -> Boolean): ControlPointBuilder {
+            callbackExecutor = taskExecutor(handler)
+            return this
+        }
+
+        /**
+         * Set whether to check the segment of SSDP Notify packet or not.
+         *
+         * @param enabled true, check the segment. false, otherwise
+         * @return builder
+         */
+        fun setNotifySegmentCheckEnabled(enabled: Boolean): ControlPointBuilder {
+            notifySegmentCheckEnabled = enabled
+            return this
+        }
+
+        /**
+         * Build an instance of ControlPoint.
+         *
+         * @return instance of ControlPoint
+         */
+        fun build(): ControlPoint = ControlPointImpl(
+            protocol,
+            getDefaultInterfacesIfEmpty(protocol, interfaces),
+            notifySegmentCheckEnabled,
+            DiFactory(protocol, callbackExecutor)
+        )
     }
 }
