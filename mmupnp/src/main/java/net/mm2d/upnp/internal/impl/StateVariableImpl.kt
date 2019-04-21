9a055331bb4c03e43710c6fcbbcf7f2fc5c28d3d
OHMAE Ryosuke <ryo@mm2d.net>
2019-04-21 22:02:51 +0900

    :memo: remove unnecessary comment

--- mmupnp/src/main/java/net/mm2d/upnp/internal/impl/StateVariableImpl.kt
+++ mmupnp/src/main/java/net/mm2d/upnp/internal/impl/StateVariableImpl.kt
@@ -8,8 +8,6 @@
 package net.mm2d.upnp.internal.impl
 
 import net.mm2d.upnp.StateVariable
-import net.mm2d.upnp.internal.parser.DeviceParser
-import net.mm2d.upnp.internal.parser.ServiceParser
 
 /**
  * [StateVariable]の実装
@@ -27,13 +25,6 @@ internal class StateVariableImpl(
     override val maximum: String?,
     override val step: String?
 ) : StateVariable {
-
-    /**
-     * ServiceDescriptionのパース時に使用するビルダー
-     *
-     * @see DeviceParser.loadDescription
-     * @see ServiceParser.loadDescription
-     */
     internal class Builder {
         private var sendEvents = true
         private var multicast = false
@@ -45,11 +36,6 @@ internal class StateVariableImpl(
         private var maximum: String? = null
         private var step: String? = null
 
-        /**
-         * [StateVariable]のインスタンスを作成する。
-         *
-         * @return [StateVariable]のインスタンス
-         */
         fun build(): StateVariable {
             val name = name
                 ?: throw IllegalStateException("name must be set.")
@@ -69,97 +55,48 @@ internal class StateVariableImpl(
             )
         }
 
-        /**
-         * SendEventsの値を登録する。
-         *
-         * 値が"no"でなければyesであると判定する。
-         *
-         * @param sendEvents SendEventsの値
-         */
+        // 値が"no"でなければyesであると判定する。
         fun setSendEvents(sendEvents: String): Builder {
             this.sendEvents = !sendEvents.equals("no", ignoreCase = true)
             return this
         }
 
-        /**
-         * Multicastの値を登録する
-         *
-         * 値が"yes"でなければnoであると判定する。
-         * なお、Multicastの受信には非対応である。
-         *
-         * @param multicast Multicastの値
-         */
+        // 値が"yes"でなければnoであると判定する。Multicastの受信には非対応。
         fun setMulticast(multicast: String): Builder {
             this.multicast = multicast.equals("yes", ignoreCase = true)
             return this
         }
 
-        /**
-         * StateVariable名を登録する。
-         *
-         * @param name StateVariable名
-         */
         fun setName(name: String): Builder {
             this.name = name
             return this
         }
 
-        /**
-         * DataTypeを登録する。
-         *
-         * @param dataType DataType
-         */
         fun setDataType(dataType: String): Builder {
             this.dataType = dataType
             return this
         }
 
-        /**
-         * AllowedValueを登録する。
-         *
-         * @param value AllowedValue
-         */
         fun addAllowedValue(value: String): Builder {
             allowedValueList.add(value)
             return this
         }
 
-        /**
-         * DefaultValueを登録する。
-         *
-         * @param defaultValue DefaultValue
-         */
         fun setDefaultValue(defaultValue: String): Builder {
             this.defaultValue = defaultValue
             return this
         }
 
-        /**
-         * Minimumを登録する。
-         *
-         * @param minimum Minimum
-         * @return Builder
-         */
         fun setMinimum(minimum: String): Builder {
             this.minimum = minimum
             return this
         }
 
-        /**
-         * Maximumを登録する。
-         *
-         * @param maximum Maximum
-         */
         fun setMaximum(maximum: String): Builder {
             this.maximum = maximum
             return this
         }
 
-        /**
-         * Stepを登録する。
-         *
-         * @param step Step
-         */
         fun setStep(step: String): Builder {
             this.step = step
             return this
