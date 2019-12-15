62f462beb8950eed2e1cb4f3498f8860bf1ec9d3
OHMAE Ryosuke <ryo@mm2d.net>
2019-12-15 23:26:57 +0900

    📖fix kdoc

--- mmupnp/src/main/java/net/mm2d/upnp/cp/internal/impl/ActionInvokeDelegate.kt
+++ mmupnp/src/main/java/net/mm2d/upnp/cp/internal/impl/ActionInvokeDelegate.kt
@@ -193,7 +193,7 @@ internal class ActionInvokeDelegate(
     /**
      * Convert XML Document to String.
      *
-     * @param document XML Document to convert
+     * @receiver document XML Document to convert
      * @return Converted string
      * @throws TransformerException If a conversion error occurs
      */
